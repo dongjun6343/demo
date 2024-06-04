212b25d7bbd083c6375aaf5a9539f92757645c5c
dongjun6343 <dongjun6343@naver.com>
2024-06-04 18:58:41 +0900

    ⚡️@EnableScheduling 스케쥴링을 하기 위해서 추가 enabled: false -> 스케쥴링을 사용한다면 false처리

--- src/main/java/com/batch/demo/DemoApplication.java
+++ src/main/java/com/batch/demo/DemoApplication.java
@@ -1,11 +1,12 @@
 package com.batch.demo;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableBatchProcessing 2.x 에서는 사용하지만, 3.x 부터는 Spring Batch 기본설정이 되므로 삭제
+@EnableScheduling // 스케쥴링을 하기 위해서 추가
 @SpringBootApplication
+//@EnableBatchProcessing 2.x 에서는 사용하지만, 3.x 부터는 Spring Batch 기본설정이 되므로 삭제
 public class DemoApplication {
 
     public static void main(String[] args) {
