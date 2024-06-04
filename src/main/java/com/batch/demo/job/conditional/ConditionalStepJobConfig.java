f747c686a042f7d9398fe47f989168f51560981c
dongjun6343 <dongjun6343@naver.com>
2024-06-04 11:24:57 +0900

    ⚡️상태값에 따른 분기처리

--- src/main/java/com/batch/demo/job/conditional/ConditionalStepJobConfig.java
+++ src/main/java/com/batch/demo/job/conditional/ConditionalStepJobConfig.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -21,26 +22,86 @@ import org.springframework.transaction.PlatformTransactionManager;
 @RequiredArgsConstructor
 public class ConditionalStepJobConfig {
 
+    /**
+     * desc: 상태값에 따른 분기처리
+     * run param: --job.name=ConditionalStepJob
+     */
     @Bean
-    public Job helloWorldJob(JobRepository jobRepository, Step simpleStep1) {
-        return new JobBuilder("helloWorldJob", jobRepository)
-                .start(simpleStep1)
+    public Job ConditionalStepJob(JobRepository jobRepository, Step conditionalStartStep, Step conditionalAllStep, Step conditionalFailStep, Step conditionalCompletedStep) {
+        return new JobBuilder("ConditionalStepJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(conditionalStartStep).on("FAILED").to(conditionalFailStep) // 실패 시
+                .from(conditionalStartStep).on("COMPLETED").to(conditionalCompletedStep) // 성공 시
+                .from(conditionalStartStep).on("*").to(conditionalAllStep) // 예상했던 실패나 성공이 아닌 값이 나올 경우
+                .end()
                 .build();
     }
 
     @Bean
     @JobScope
-    public Step simpleStep1(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager) {
-        return new StepBuilder("simpleStep1", jobRepository)
-                .tasklet(testTasklet, platformTransactionManager)
+    public Step conditionalStartStep(JobRepository jobRepository, Tasklet conditionalStartStepTasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("conditionalStartStep", jobRepository)
+                .tasklet(conditionalStartStepTasklet, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step conditionalFailStep(JobRepository jobRepository, Tasklet conditionalFailStepTasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("conditionalFailStep", jobRepository)
+                .tasklet(conditionalFailStepTasklet, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step conditionalCompletedStep(JobRepository jobRepository, Tasklet conditionalCompletedStepTasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("conditionalCompletedStep", jobRepository)
+                .tasklet(conditionalCompletedStepTasklet, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step conditionalAllStep(JobRepository jobRepository, Tasklet conditionalAllStepTasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("conditionalAllStep", jobRepository)
+                .tasklet(conditionalAllStepTasklet, platformTransactionManager)
                 .build();
     }
 
     @Bean
     @StepScope
-    public Tasklet testTasklet() {
+    public Tasklet conditionalStartStepTasklet() {
         return ((contribution, chunkContext) -> {
-            log.info(">>>>>>>>> Hello World Step1");
+            System.out.println("conditional Start Step !!!");
+            return RepeatStatus.FINISHED; // 정상적으로 처리했으므로 Completed Step 실행
+            //throw new Exception("ERROR!!!"); // 비정상적인 에러 발생 -> Fail Step 실행
+        });
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet conditionalFailStepTasklet() {
+        return ((contribution, chunkContext) -> {
+            System.out.println("conditional Fail Step !!!");
+            return RepeatStatus.FINISHED;
+        });
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet conditionalCompletedStepTasklet() {
+        return ((contribution, chunkContext) -> {
+            System.out.println("conditional Completed Step !!!");
+            return RepeatStatus.FINISHED;
+        });
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet conditionalAllStepTasklet() {
+        return ((contribution, chunkContext) -> {
+            System.out.println("conditional All Step !!!");
             return RepeatStatus.FINISHED;
         });
     }
